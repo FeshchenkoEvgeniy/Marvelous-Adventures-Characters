cabfeca316c9e6b6f8d28d7802754f9af20acc8c
Evgeniy <feshchenko.evgeniy03@gmail.com>
2024-09-23 13:43:07 +0300

    update some clasess

--- src/js/swiper.ts
+++ src/js/swiper.ts
@@ -1,12 +1,12 @@
 import Swiper from 'swiper';
 import 'swiper/css';
-// import 'swiper/css/pagination';
 import {Pagination} from 'swiper/modules';
+
+const heroBtn = document.querySelector('.js-hero__btn') as HTMLElement
 
 const swiper = new Swiper('.swiper', {
   modules: [Pagination],
-  slidesPerView: 1, // або 2, 3 - відповідно до вашого макету
-  // spaceBetween: 10,
+  slidesPerView: 1,
   direction: 'horizontal',
   pagination: {
     el: '.swiper-pagination',
@@ -20,4 +20,33 @@ const swiper = new Swiper('.swiper', {
       direction: 'vertical',
     }
   },
+  on: {
+    slideChange: function () {
+      const bullets = document.querySelectorAll('.swiper-pagination-bullet');
+      bullets.forEach((bullet) => {
+        bullet.classList.remove('swiper-pagination-bullet-active--red', 'swiper-pagination-bullet-active--green');
+      });
+
+      const swiperActiveBullet = document.querySelector('.swiper-pagination-bullet-active') as HTMLElement;
+      console.log(swiperActiveBullet)
+      switch (this.activeIndex) {
+        case 0:
+          heroBtn.style.backgroundColor = 'rgba(52, 56, 127, 1)';
+          break;
+        case 1:
+          heroBtn.style.backgroundColor = 'rgba(96, 4, 4, 1)';
+          if (swiperActiveBullet) {
+            swiperActiveBullet.classList.add('swiper-pagination-bullet-active--red');
+          }
+          break;
+        case 2:
+          heroBtn.style.backgroundColor = 'rgba(91, 127, 60, 1)';
+          if (swiperActiveBullet) {
+            swiperActiveBullet.classList.add('swiper-pagination-bullet-active--green');
+          }
+          break;
+      }
+      console.log('Слайд змінився на: ', this.activeIndex);
+    },
+  }
 });
