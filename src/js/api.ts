85786fba417788ebee0b7a181a035c9b91c6e7ed
Evgeniy <feshchenko.evgeniy03@gmail.com>
2024-09-27 21:56:03 +0300

    add new fetch

--- src/js/api.ts
+++ src/js/api.ts
@@ -5,7 +5,7 @@ const ts = '1'
 const publickKey = '3e4a92df9169701b297c3638807c7b2e'
 const privateKey = 'c93b62455441ec9a036d868875eb8644bb02aa07'
 const hash = md5(ts + privateKey + publickKey)
-const BASE_URL = 'https://gateway.marvel.com/v1/public/'
+const BASE_URL = 'https://gateway.marvel.com/v1/public'
 
 const fetchFiveRandomCharacters = async () => {
     const characterIds = randomCharactersId();
@@ -20,4 +20,9 @@ const fetchFiveRandomCharacters = async () => {
     return randomCharacters
 };
 
-export {fetchFiveRandomCharacters}
+async function fetchAllCharacters(offset:number, limit:number) {
+    const response = await fetch(`${BASE_URL}/characters?limit=${limit}&offset=${offset}&ts=${ts}&apikey=${publickKey}&hash=${hash}`);
+    return response.json();
+}
+
+export {fetchFiveRandomCharacters, fetchAllCharacters}
