5b0af4e377caf4d19cdc181807e7e4c26e631ae8
Evgeniy <feshchenko.evgeniy03@gmail.com>
2024-10-07 12:46:20 +0300

    add new interface

--- src/js/interface.ts
+++ src/js/interface.ts
@@ -25,4 +25,28 @@ interface IfilterObj {
     date: null | string,
 }
 
-export { ICharacters, IinitialState, IfilterObj}
+interface IRandomComics {
+    thumbnail: IThumbnail,
+    title: string,
+}
+
+interface IDetailCharacters {
+    name: string,
+    description: string,
+    modified: string,
+    thumbnail: IThumbnail,
+    randomComics: IRandomComics[]
+}
+
+interface IComics {
+  resourceURI: string;
+  name: string;
+}
+
+interface IResult {
+  comics: {
+    items: IComics[];
+  };
+}
+
+export { ICharacters, IinitialState, IfilterObj, IDetailCharacters, IResult}
