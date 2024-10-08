9663c5aa06800ea3f24d3073967d49c1c44faa22
Evgeniy <feshchenko.evgeniy03@gmail.com>
2024-10-08 13:53:24 +0300

    Update createMarkup.ts

--- src/js/createMarkup.ts
+++ src/js/createMarkup.ts
@@ -1,6 +1,6 @@
-import {ICharacters} from './interface'
+import {ICharacters, IDetailCharacters} from './interface'
 
-function createMarkupForSwiperWrapper(arr: ICharacters[]) {
+function createMarkupForSwiperWrapper(arr: ICharacters[]): string {
     return arr.map(({name, thumbnail:{extension,path}}: ICharacters) => {
         return `
         <div class="swiper-slide"><img src="${path}.${extension}" alt="${name}" class='random-characters__img'></div>
@@ -10,7 +10,7 @@ function createMarkupForSwiperWrapper(arr: ICharacters[]) {
 
 const str: string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero praesentium doloremque quos voluptatem voluptates ipsa recusandae fugit officia. Sequi, cupiditate ut. Error placeat reprehenderit laudantium quibusdam voluptates,laboriosam ipsum!'
 
-function createMarkupForRandomCharacters(arr: ICharacters[]) {
+function createMarkupForRandomCharacters(arr: ICharacters[]): string {
     return arr.map(({ name, description, resourceURI }: ICharacters) => {
         return `<li class="random-characters__item" data-url='${resourceURI}'>
         <p class='random-characters__name'>${name}</p>        
@@ -30,5 +30,40 @@ function createMarkupForMainCharactersList(arr: ICharacters[]): string {
     }).join('');
 }
 
+function createMarkupForModalCharacters(data: IDetailCharacters): string {
+    const { name, description, modified, thumbnail: { path, extension }, randomComics } = data
 
-export {createMarkupForRandomCharacters, createMarkupForSwiperWrapper, createMarkupForMainCharactersList}
+    const date = new Date(modified)
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = date.toLocaleDateString('en-US', options);
+
+    const markup = randomComics.map(({thumbnail: {path, extension}, title}) => {
+        return `<li class='characters-modal__item'>
+                <img src="${path}.${extension}" alt="${title}" class='characters-modal__comics-img'>
+                <p class='characters-modal__comics-title'>${title}</p>
+            </li>`    
+        }).join('')
+    return `
+    <div class='characters-modal__container'>
+        <img src="${path}.${extension}" alt="${name}" class='characters-modal__img'>
+    <div class='characters-modal--bg'>
+    <div class='characters-modal__box'>
+        <div class='characters-modal__wrapper'> 
+        <p class='characters-modal__name'>${name}</p>
+        <p class='characters-modal__modified'>${formattedDate}</p>
+        </div>
+        <p class='characters-modal__description'>${!description.trim().length || description.includes('&nbsp;') ? str : description}</p>
+    </div>
+    <div>
+        <p class='characters-modal__title'>List of comics</p>
+        <ul class='characters-modal__list'>
+            ${markup}
+        </ul>
+    </div>
+    </div>
+    </div>    
+    `
+}
+
+
+export {createMarkupForRandomCharacters, createMarkupForSwiperWrapper, createMarkupForMainCharactersList, createMarkupForModalCharacters}
