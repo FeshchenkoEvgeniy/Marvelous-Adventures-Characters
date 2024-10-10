0d7ada3ba8b23833fdd499b02a056afcd96dc971
Evgeniy <feshchenko.evgeniy03@gmail.com>
2024-10-10 13:45:59 +0300

    Update mainCharactersModal.ts

--- src/js/mainCharactersModal.ts
+++ src/js/mainCharactersModal.ts
@@ -2,9 +2,28 @@ import * as basicLightbox from 'basiclightbox'
 import 'basiclightbox/dist/basicLightbox.min.css'
 import { fetchCharactersById } from "./api";
 import { createMarkupForModalCharacters } from './createMarkup';
+import { isLoad } from './mainCharactersComicsModal';
 
 const charactersList = document.querySelector('.js-characters-list') as HTMLElement;
 const body = document.querySelector('body') as HTMLElement
+
+let handler: (evt: KeyboardEvent) => void;
+const options = {
+    onShow(instance: any) {
+        handler = closeModal.bind(instance);
+   
+        document.addEventListener('keydown', handler);
+        body.style.overflowY = 'hidden' 
+            
+        return true
+    },
+    onClose() {
+        body.style.overflowY = ''
+        document.removeEventListener('keydown', handler);
+
+        return true
+    }
+}
 
 function onClick(evt: Event) {
     const target = evt.target as HTMLElement
@@ -17,29 +36,18 @@ function onClick(evt: Event) {
     const url = listItem?.dataset.url
 
     if (url) {
+    body.style.pointerEvents = 'none'
+ 
     fetchCharactersById(url)
-    .then((data) => {
-    let handler: (evt: KeyboardEvent) => void;  
-    const instance = basicLightbox.create(createMarkupForModalCharacters(data),{
-    onShow(instance) {
-        handler = closeModal.bind(instance);
-            
-        document.addEventListener('keydown', handler);
-        body.style.overflowY = 'hidden' 
-            
-        return true
-    },
-    onClose() {
-        body.style.overflowY = ''
-        document.removeEventListener('keydown', handler);
-
-        return true
-    },            
-    })
-        
+    .then((data) => {    
+    const instance = basicLightbox.create(createMarkupForModalCharacters(data),options)
     instance.show()   
     })
     .catch(err => console.log(err))
+    .finally(() => {
+    body.style.pointerEvents = 'all'
+    isLoad()
+    })
     } else {
         console.error('URL not found');
     }
